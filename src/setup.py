69a740657e61bcd8b586353fe25abf80581acc0b
Charles Williams <C.Williams@gns.cri.nz>
2024-05-16 12:14:22 +1200

    Added some example VTK files and modified setup.py.

--- src/setup.py
+++ src/setup.py
@@ -7,11 +7,21 @@ setup(name='fault_mesh_tools',
       author_email='c.williams@gns.cri.nz',
       packages=find_packages(),
       package_data={'fault_mesh_tools': ['data/*']},
+      scripts=['fault_mesh_tools/cli/fmt_fault_global_to_local',
+               'fault_mesh_tools/cli/fmt_fault_local_to_global',
+               'fault_mesh_tools/cli/fmt_ts_fault_local_to_cubit',
+               'fault_mesh_tools/cli/fmt_ts_fault_local_to_cubit_nurbs',
+               'fault_mesh_tools/cli/fmt_tsurf2vtk'],
       requires=[
           'numpy',
           'scipy',
           'shapely',
           'meshio',
           'rsqsim_api',
-          'pathlib']
+          'pathlib',
+          'argparse',
+          'pickle',
+          'sys',
+          'six',
+          'os']
       )
